1661a7f1ad21c94abce9fd18d669c0b6febd6e48
Ivan Hrekov <iandrc9@gmail.com>
2026-03-02 12:59:20 +0100

    feat: add basic enter logic

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -6,13 +6,20 @@ pragma solidity ^0.8.33;
 /// @notice This contract is for creating a sample raffle (lottery)
 /// @dev Implements Chainlink VRFv2.5
 contract Raffle {
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 fee) {
         i_entranceFee = fee;
     }
 
-    function enter() public payable {}
+    function enter() public payable {
+        require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle());
+
+        s_players.push(payable(msg.sender));
+    }
 
     function pickWinner() public {}
 
