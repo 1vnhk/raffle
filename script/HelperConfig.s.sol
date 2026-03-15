b937458b681ae6b1f3f919596ca03e78ab30fc1f
Ivan Hrekov <iandrc9@gmail.com>
2026-03-15 21:53:24 +0100

    fix: set callback gas limit to smaller amount

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -58,7 +58,7 @@ contract HelperConfig is Script, CodeConstants {
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 85516360159834589453939043734551559605371279717820097179718689774374385930779,
-            callbackGasLimit: 2_500_000,
+            callbackGasLimit: 100_000,
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
             account: vm.envOr("SEPOLIA_ACCOUNT", DEFAULT_SENDER)
         });
